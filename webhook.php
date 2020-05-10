cd085befd2f5e7f234ae2c20cb469f5b0a969394
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 13:19:48 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,7 +5,9 @@ const BASE_URL = 'https://api.telegram.org/bot'.TOKEN.'/';
 
 $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
-function sendRequest($method, $params = []) {
+function sendRequest($method, $params = []) 
+
+{
 
 	if (!empty($params)) {
 		$url = BASE_URL.$method.'?'. http_build_query($params);
@@ -18,9 +20,9 @@ function sendRequest($method, $params = []) {
 }
 
 $time = date('H:m:s');
-$chat_id = $update['massge']['chat']['id'];
+$chat_id = $update['message']['chat']['id'];
 
-sendRequest('sendMassage',['chat_id'=> $chat_id, 'text'=> $time]);
+sendRequest('sendMessage',['chat_id'=> $chat_id, 'text'=> $time]);
 
 
 
