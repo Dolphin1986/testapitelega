250d32df2993cca90486f8634c7a16cb4db12934
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 13:16:07 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,17 +1,33 @@
 <?php
 
-// $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-
+const TOKEN = '1228487916:AAHi6eOerKee0pp20YVbUgei57DILMQe33Y';
+const BASE_URL = 'https://api.telegram.org/bot'.TOKEN.'/';
 
 $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
+function sendRequest($method, $params = []) {
+
+	if (!empty($params)) {
+		$url = BASE_URL.$method.'?'. http_build_query($params);
+	} else {
+		$url = BASE_URL.$method;
+	}
+
+	return json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+
+}
+
+$time = date('H:m:s');
+$chat_id = $update['massge']['chat']['id'];
+
+sendRequest('sendMassage',['chat_id'=> $chat_id, 'text'=> $time]);
+
+
+
+
 // $time = file_put_contents(__DIR__.'/log.txt', $update['update_id']);
 
-
-file_put_contents(__DIR__.'/last_log.txt', file_get_contents('php://input');
-
-
-
+// file_put_contents(__DIR__.'/last_log.txt', file_get_contents('php://input');
 // if ($time = $time) {
 // 	file_put_contents(__DIR__.'/last_log.txt', $update['0']['img_url']);
 // } else {
