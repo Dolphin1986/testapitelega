9430271064701d07daaff3337d1705db9a87cb30
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 12:40:15 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@
 
 $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
-file_put_contents(__DIR__.'/last_log.txt', $update['0']['img_url']);
+file_put_contents(__DIR__.'/last_log.txt', $update);
 
 
 
