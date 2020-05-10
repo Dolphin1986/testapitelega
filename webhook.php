93374cc3735d39771a9d7698d231443fecc06d6d
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 13:36:03 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,10 +5,10 @@ const BASE_URL = 'https://api.telegram.org/bot'.TOKEN.'/';
 
 $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
-
-$update_id = file_put_contents(__DIR__.'/log.txt', $update['update_id']);
-
 file_put_contents(__DIR__.'/log.txt', file_get_contents('php://input'));
+
+$update_id = $update['update_id'];
+
 
 if ($update_id = $update_id) {
 	file_put_contents(__DIR__.'/last_log.txt', $update['message']['chat']['id']);
