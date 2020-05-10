09b569f73d4e6cf18d416cbde26e954c1430ac6e
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-10 12:42:25 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,7 +5,8 @@
 
 $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
 
-file_put_contents(__DIR__.'/last_log.txt', $update);
+
+file_put_contents(__DIR__.'/last_log.txt', file_get_contents('php://input'));
 
 
 
