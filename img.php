609e4a67c07d8e4e3387ef4297f7f5cf9c1d5a15
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 19:37:47 +0300

    Update img.php

--- img.php
+++ img.php
@@ -4,5 +4,11 @@ $data = file_get_contents(__DIR__.'/log.txt',true);
 // $img = file_get_contents(__DIR__.'/img.txt',true);
 
 // echo '<img src="'.$img.'">';
-echo "<h1>".$data."</h1>";
+
+if ($data = $data){
+	echo "<h1>".$data."</h1>";
+
+} else {
+	echo "<h1>".$data."</h1>";
+}
 
