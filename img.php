3d81baffc9f33e70b5d57fd7fb2f0511cdd8d7fc
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 19:05:24 +0300

    Update img.php

--- img.php
+++ img.php
@@ -1,7 +1,8 @@
 <?php
 
-// $data = file_get_contents(__DIR__.'/log.txt',true);
-$img = file_get_contents(__DIR__.'/img.txt',true);
+$data = file_get_contents(__DIR__.'/log.txt',true);
+// $img = file_get_contents(__DIR__.'/img.txt',true);
 
-echo '<img src="'.$img.'">';
+// echo '<img src="'.$img.'">';
+echo "<h1>".$date."</h1>";
 
