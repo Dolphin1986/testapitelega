0e5a5b90638754251a5fb7ec5f05a15bdf9908cf
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 19:02:40 +0300

    05

---
+++ img.php
@@ -0,0 +1,7 @@
+<?php
+
+// $data = file_get_contents(__DIR__.'/log.txt',true);
+$img = file_get_contents(__DIR__.'/img.txt',true);
+
+echo '<img src="'.$img.'">';
+
--- index.php
+++ index.php
@@ -1,47 +1,32 @@
-<?php
-
-$data = file_get_contents(__DIR__.'/log.txt',true);
-$img = file_get_contents(__DIR__.'/img.txt',true);
-
-
-
-?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Test Slider</title>
-	<script type="text/javascript">
-		function nikRefresh()  
-					{  
-					    jQuery.ajax({  
-					        url: "hook.php",
-					        type : 'get',
-					        success: function(html){  
-					            jQuery("#content").html(jQuery('.data', data).html());
-					        }  
-					    });  
-					}
-
-	</script>
-</head>
-	
-	<body>
-		<h2>Текст сообщения вот: </h2>
-		<?php
-			echo ($data);
-		?>
-		<br>
-		<h1>Тут должна быть картинка:</h1>
-		<div class="data">
-			<img src="<?php
-				echo ($img);
-			?>">
-		</div>
-	</body>
-
-
-
-
+<html>  
+<head>  
+<meta http-equiv="Content-Type" content="text/html; Charset=UTF-8">  
+<script type="text/javascript" src="jquery.js"></script>  
+</head>  
+  
+<body>  
+      
+    <div id="content">
+    	
+    </div>  
+      
+    <script>  
+        function show()  
+        {  
+            $.ajax({  
+                url: "img.php",  
+                cache: false,  
+                success: function(html){  
+                    $("#content").html(html);  
+                }  
+            });  
+        }  
+      
+        $(document).ready(function(){  
+            show();  
+            setInterval('show()',1000);  
+        });  
+    </script>  
+      
+</body>  
 </html>
