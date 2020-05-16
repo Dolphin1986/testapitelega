96260e202ffd834cadde3a00e95e96829a80bdec
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-16 11:08:26 +0300

    03

--- img.php
+++ img.php
@@ -1,15 +1,11 @@
 <?php
 
 $data = file_get_contents(__DIR__.'/log.txt',true);
-// $img = file_get_contents(__DIR__.'/img.txt',true);
+$img = file_get_contents(__DIR__.'/img.txt',true);
 
-// echo '<img src="'.$img.'">';
+echo '<img src="'.$img.'">';
 
-echo "<h1>".$data."</h1>";
-echo "<h1>".$data."</h1>";
-echo "<h1>".$data."</h1>";
-echo "<h1>".$data."</h1>";
-
+// echo "<h1>".$data."</h1>";
 
 
 
