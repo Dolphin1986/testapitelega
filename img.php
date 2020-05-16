1ca38e54c32055cc230745cf1341051d2bfd72b9
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-16 14:43:09 +0300

    Update img.php

--- img.php
+++ img.php
@@ -5,7 +5,10 @@ $img = file_get_contents(__DIR__.'/img.txt',true);
 
 print_r($img);
 
-echo '<img src="'.$img.'">';
+foreach ($img as $key => $value) {
+	echo '<img src="'.$value.'">';
+}
+
 
 // echo "<h1>".$data."</h1>";
 
